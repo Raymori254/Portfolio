95e241cc4bd4efb64af15055385505484bf53ab1
ray <raymori254@gmail.com>
2025-07-01 16:34:33 +0300

    initiate commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Contact from "../components/contact";
+import Footer from "../components/footer";
+import Hero from "../components/hero";
+import NavBar from "../components/navbar";
+import Resume from "../components/resume";
+import Skills from "../components/skills";
+import Work from "../components/work";
+
+const Home = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  function toggleDarkMode() {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  }
+  return (
+    <>
+      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Skills darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Work darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Resume darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Contact darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+    </>
+  );
+};
+
+export default Home;
